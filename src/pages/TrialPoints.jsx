e98a8e467bcaa7e58e7f7959301cee5465e92396
mellomee2 <mellomee2@gmail.com>
2026-02-23 20:35:11 +0000

    File changes

--- src/pages/TrialPoints.jsx
+++ src/pages/TrialPoints.jsx
@@ -52,6 +52,7 @@ export default function TrialPoints() {
   const [draggingId, setDraggingId] = useState(null);
 
   const load = async () => {
+
     if (!activeCase) return;
     const [pts, cats] = await Promise.all([
       base44.entities.TrialPoints.filter({ case_id: activeCase.id }),
