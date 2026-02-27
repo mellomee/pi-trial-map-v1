03d6aff09be9ce8b8a3c62d2670b2a0f88d5f091
mellomee2 <mellomee2@gmail.com>
2026-02-27 19:02:04 +0000

    File changes

--- src/pages/VideoHub.jsx
+++ src/pages/VideoHub.jsx
@@ -10,6 +10,7 @@ import {
   ExternalLink, Check, X, Edit2
 } from "lucide-react";
 import { createPageUrl } from "@/utils";
+import { Link } from "react-router-dom";
 import useActiveCase from "@/components/hooks/useActiveCase";
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
