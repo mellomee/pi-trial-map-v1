93bc053ce3d0f5cb8f43234d1e4ff5ef5c8dde2a
mellomee2 <mellomee2@gmail.com>
2026-03-01 21:34:04 +0000

    File changes

--- src/pages/AnnotatePage.jsx
+++ src/pages/AnnotatePage.jsx
@@ -9,6 +9,7 @@ import {
 import useActiveCase from "@/components/hooks/useActiveCase";
 import { createPageUrl } from "@/utils";
 import QuoteAnnotationModal from "@/components/annotate/QuoteAnnotationModal";
+import FindOnPage from "@/components/annotate/FindOnPage";
 import { pdfjs } from "react-pdf";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
