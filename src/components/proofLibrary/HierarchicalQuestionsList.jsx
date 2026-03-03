16f1b57d182498bc8c88e44e2438b7fbc2652952
mellomee2 <mellomee2@gmail.com>
2026-03-03 23:56:00 +0000

    File changes

--- src/components/proofLibrary/HierarchicalQuestionsList.jsx
+++ src/components/proofLibrary/HierarchicalQuestionsList.jsx
@@ -14,6 +14,7 @@ import AddQuestionProofModal from '@/components/proofLibrary/AddQuestionProofMod
 export default function HierarchicalQuestionsList({
   questions,
   evidenceGroupId,
+  evidenceGroup,
   caseId,
   proofItems,
   calloutNames,
