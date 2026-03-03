9ecbe5b9300406000d2a25f899691995a164414f
mellomee2 <mellomee2@gmail.com>
2026-03-03 21:23:12 +0000

    File changes

--- src/components/proofLibrary/AddQuestionProofModal.jsx
+++ src/components/proofLibrary/AddQuestionProofModal.jsx
@@ -6,6 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Input } from '@/components/ui/input';
 import { Badge } from '@/components/ui/badge';
 import { Search, ChevronLeft, ChevronRight, ZoomIn, ZoomOut, FileText, Image, Eye } from 'lucide-react';
+import LinkQuestionProofModal from '@/components/proofLibrary/LinkQuestionProofModal';
 import * as pdfjs from 'pdfjs-dist';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
