8d5e48fabceee4cbbf2bc04b7ce7600f2e4c97bb
mellomee2 <mellomee2@gmail.com>
2026-03-08 15:51:00 +0000

    File changes

--- src/components/shared/PdfViewer.jsx
+++ src/components/shared/PdfViewer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useImperativeHandle } from 'react';
 import * as pdfjsLib from 'pdfjs-dist';
 import { ChevronLeft, ChevronRight, ZoomIn, ZoomOut } from 'lucide-react';
 import { Button } from '@/components/ui/button';
